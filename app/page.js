e6d943e5d67ea2510f74f9fc291d4d3a112ad3c5
jate <jenkij1159@gmail.com>
2026-02-28 22:39:39 +0700

    chore: init nextjs project

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Team Portfolio</h1>
+      <p>Welcome to our team website.</p>
+      <ul>
+        <li><a href="/member-a">Member A</a></li>
+        <li><a href="/member-b">Member B</a></li>
+      </ul>
+    </main>
+  );
+}
