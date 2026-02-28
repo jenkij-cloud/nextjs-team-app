6b282befe88189051d5061f1a47af0556d0830c5
jate <jenkij1159@gmail.com>
2026-02-28 23:53:17 +0700

    feat: update home page links

--- app/member-b/page.js
+++ app/member-b/page.js
@@ -2,8 +2,8 @@ export default function MemberB() {
   return (
     <main style={{ padding: 24 }}>
       <h1>Member B Profile</h1>
-      <p><b>Name:</b> [ชื่อ-นามสกุล ของเพื่อนคนที่ 2]</p>
-      <p><b>Student ID:</b> [รหัสนิสิต]</p>
+      <p><b>Name:</b> [นายชัชพล พรหมขวัญ]</p>
+      <p><b>Student ID:</b> [6720210036]</p>
       <p><b>Role:</b> Developer</p>
       <hr />
       <a href="/"> กลับหน้าหลัก </a>
--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main style={{ padding: 24 }}>
       <h1>Team Portfolio</h1>
       <p>Welcome to our team website.</p>
-      <ul>
-        <li><a href="/member-a">Member A</a></li>
-        <li><a href="/member-b">Member B</a></li>
+      
+      <ul style={{ marginTop: 20, lineHeight: '2' }}>
+        <li>
+          <Link href="/">Member A (เจต)</Link> 
+        </li>
+        <li>
+          <Link href="/member-b">Member B (ปอ)</Link>
+        </li>
       </ul>
     </main>
   );
